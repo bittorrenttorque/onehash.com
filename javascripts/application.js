2f6bd379951bbee6c53ca66dc98c40d8f4f0cfd6
Patrick Williams <patrick@bittorrent.com>
2012-07-11 14:52:04 -0700

    add more event listeners and use utorrent instead of torque

--- javascripts/application.js
+++ javascripts/application.js
@@ -60,8 +60,29 @@ jQuery(function() {
         },
         bindPlayerEvents: function(player) {
             _.each(EVENTS, function(event) {
-                player.addEvent(event, _.bind(this.onPlayerEvent, this, event));
+                //player.addEvent(event, _.bind(this.onPlayerEvent, this, event));
             }, this);
+
+            var video = this.$el.find('video')[0];
+            video.addEventListener("readystatechange", function(evt) { console.log('readystatechange'); } );
+            video.addEventListener("stalled", function(evt) { console.log("stalled",evt); } );
+            video.addEventListener("durationchange", function(evt) { console.log('durationchange',evt); } );
+            video.addEventListener("loadstart", function(evt) { console.log("load start",evt); } );
+            video.addEventListener("abort", function(evt) { console.log("abort",evt); } );
+            video.addEventListener("loadedmetadata", function(evt) { console.log("got metadata",evt); } );
+            video.addEventListener("error", function(evt) { 
+                console.log("got error", evt); 
+                console.log('video state: ',video.readyState); 
+            } );
+            video.addEventListener("canplay", function(evt) { console.log('canplay',evt); } );
+            video.addEventListener("progress", function(evt) { console.log("progress"); } );
+            video.addEventListener("seek", function(evt) { console.log('seek',evt); } );
+            video.addEventListener("seeked", function(evt) { console.log('seeked',evt); } );
+            video.addEventListener("ended", function(evt) { console.log('ended',evt); } );
+            //video.addEventListener("timeupdate", function(evt) { console.log('timeupdate',evt); } );
+            video.addEventListener("pause", function(evt) { console.log('pause',evt); } );
+            video.addEventListener("play", function(evt) { console.log('play',evt); } );
+            video.addEventListener("suspend", function(evt) { console.log('suspend event',evt); });
         },
         unbindPlayerEvents: function(player) {
              _.each(EVENTS, function(event) {
@@ -87,7 +108,10 @@ jQuery(function() {
     if(window.location.hash) {
         window.btapp = new Btapp();
         btapp.on('all', _.bind(console.log, console));
-        btapp.connect();
+        btapp.connect({
+            product: 'uTorrent',
+            plugin: false
+        });
 
         btapp.live('torrent * file * properties', function(properties) {
             var name = properties.get('name');
