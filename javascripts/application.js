f05fc8ff32b08d5785222300040987dfbb182db0
Patrick Williams <patrick@bittorrent.com>
2012-07-25 18:00:30 -0700

    more fading

--- javascripts/application.js
+++ javascripts/application.js
@@ -483,6 +483,8 @@ jQuery(function() {
         var _this = $(this);
         _this.parent().toggleClass('expanded');
         _this.parent().toggleClass('collapsed');
+        _this.hide();
+        _this.fadeIn();
     });
 
     $(window).bind('hashchange', _.debounce(_.bind(location.reload, location)));
